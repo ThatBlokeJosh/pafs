fe46e3a0f89a43b848f381d517804aa2cb36a98d
ThatBlokeJosh <joshtomrem@protonmail.com>
2023-03-01 15:39:01 +0100

    Extension configuration and documentation

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## *An automatic :robot: downloads folder sorter writen in Python :snake: with JSON configuration*
 
+---
+
 # TODO:
 
 - [ ]  Add Windows :desktop_computer: support
@@ -9,3 +11,31 @@
 - [ ]   Add some form of system arguments :angry:
 
 - [x]  Increase configuration :gear:
+
+---
+
+# How to configure
+
+*The default path configuration should work for most Linux machines, however if you want to change the sorting path in any way then change the path in the conf.JSON file like so:*
+
+```json
+{
+    "id": "Pictures",
+    "path": "~/Personal/Pictures"
+},
+```
+
+*in this example I changed the directory in which all picture format files go*
+
+---
+
+*To change the extension list, add the extension to the category that you want to sort that file format to, for example:*
+
+```json
+{
+    "id": "Documents",
+    "extensions": ["doc", "docx", "md"]
+},
+```
+
+*in this example I added and removed some file extensions from the Documents category*
--- main.py
+++ main.py
@@ -9,36 +9,31 @@ def readConf():
         paths[0] = paths[0].replace("user", os.getlogin())
         return paths
 
-def moveFiles(paths, extensions=[".jpg", ".png", ".jpeg", ".pdf", ".docx", ".txt", ".mp4", ".mkv", ".avi", ".mp3", ".wav", ".flac"]):
+def getExt():
+    with open("conf.JSON", "r") as f:
+        data = json.load(f)["extensions"]
+
+        return data
+
+def moveFiles(paths, extensions):
     for file in os.listdir(paths[0]):
         if platform.system() == "Linux":
-            if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
+            if extensions[0]["extensions"].__contains__(file.split(".")[-1]):
                 os.system(f"mv {paths[0]}/{file} {paths[1]}/{file}")
-            elif file.endswith(".pdf") or file.endswith(".docx") or file.endswith(".txt"):
+            elif extensions[1]["extensions"].__contains__(file.split(".")[-1]):
                 os.system(f"mv {paths[0]}/{file} {paths[2]}/{file}")
-            elif file.endswith(".mp4") or file.endswith(".mkv") or file.endswith(".avi"):
+            elif extensions[2]["extensions"].__contains__(file.split(".")[-1]):
                 os.system(f"mv {paths[0]}/{file} {paths[3]}/{file}")
-            elif file.endswith(".mp3") or file.endswith(".wav") or file.endswith(".flac"):
+            elif extensions[3]["extensions"].__contains__(file.split(".")[-1]):
                 os.system(f"mv {paths[0]}/{file} {paths[4]}/{file}")
-            else:
-                print("No files to move")
-        else:
-            if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
-                os.system(f"move {paths[0]}/{file} {paths[1]}/{file}")
-            elif file.endswith(".pdf") or file.endswith(".docx") or file.endswith(".txt"):
-                os.system(f"move {paths[0]}/{file} {paths[2]}/{file}")
-            elif file.endswith(".mp4") or file.endswith(".mkv") or file.endswith(".avi"):
-                os.system(f"move {paths[0]}/{file} {paths[3]}/{file}")
-            elif file.endswith(".mp3") or file.endswith(".wav") or file.endswith(".flac"):
-                os.system(f"move {paths[0]}/{file} {paths[4]}/{file}")
-            else:
-                print("No files to move")
 
 def main():
     paths = readConf()
+    extensions = getExt()
+    print(extensions)
     print(paths)
     while True:
-        moveFiles(paths)
+        moveFiles(paths, extensions)
 
 if __name__ == "__main__":
     main()
